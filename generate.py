b7db11c30b42002d5e902507fc2aefe178efe1e8
Harihara Krishnan Narayanan <harihara@squareup.com>
2019-05-14 21:56:04 -0700

    Added ability to read input from a file

--- generate.py
+++ generate.py
@@ -22,7 +22,18 @@ def generate_draft_order(teams, num_lottery_picks):
 	return draft_order + [team.team_name for team in teams]
 
 
-if __name__ == '__main__':
+def get_file_input():
+	teams = []
+	print '\nInput file should be csv format with team name,lottery pick percentage in each line.'
+	input_file_path = raw_input('Enter file path: ')
+	with open(input_file_path) as file:
+		for line in file:
+			parts = line.strip().split(",")
+			teams.append(TeamPercentage(parts[0], int(parts[1])))
+	return teams
+
+
+def get_console_input():
 	num_teams = int(raw_input('\nEnter the number of teams: '))
 	teams = []
 	print '\nWill now prompt you to enter the {} teams in draft order'.format(num_teams)
@@ -30,11 +41,14 @@ if __name__ == '__main__':
 		team_name = raw_input('\nEnter team #{} name: '.format(num_team + 1))
 		team_lottery_pick_percentage = int(raw_input("Enter {}'s lottery pick percentage: ".format(team_name)))
 		teams.append(TeamPercentage(team_name, team_lottery_pick_percentage))
+	return teams
 
+def verify_teams_input(teams):
 	sum_lottery_pick_percentages = sum([team.team_lottery_pick_percentage for team in teams])
 	if sum_lottery_pick_percentages != 100:
 		raise Exception('Lottery pick percentages of teams: {} is not 100'.format(sum_lottery_pick_percentages))
 
+def get_num_lottery_picks_input(num_teams):
 	num_lottery_picks = int(raw_input('\nEnter the number of lottery picks: '))
 	if num_lottery_picks > num_teams:
 		raise Exception(
@@ -42,9 +56,26 @@ if __name__ == '__main__':
 				num_lottery_picks, num_teams
 			)
 		)
+	return num_lottery_picks
+
+
+if __name__ == '__main__':
+	print 'Welcome to the NBA Lottery Draft Generator'
+	print '###########################################'
+
+	input_option = raw_input('\nEnter 1 to read team input from a file or anything else to type input into the console: ').strip()
+	if input_option == '1':
+		teams = get_file_input()
+	else:
+		teams = get_console_input()
+
+	verify_teams_input(teams)
+
+	num_lottery_picks = get_num_lottery_picks_input(len(teams))
 
 	draft_order = generate_draft_order(teams, num_lottery_picks)
-	print 'Draft Order'
+	print '\nGenerated Draft Order'
 	print '########################'
 	for team in draft_order:
 		print team
+	print '\n'
